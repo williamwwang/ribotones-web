dce310e519d0f9ad1d41abbf420f9d5ddd03912c
William Wang <williamwwang314@gmail.com>
2020-10-30 23:59:38 -0400

    Alumni button

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function (){
 	var currentInstrument= 'all';
-	var showCurrentMembers = true;
+	// current, exec, alumni
+	var memberSet = 'current';
+	var instrumentTagsStr = '#show-all, #show-piano, #show-strings, #show-winds, #show-triangle';
 	var $instrumentTags = $('#show-all, #show-piano, #show-strings, #show-winds, #show-triangle');
 	var $memberTags = $('#show-current, #show-alumni');
 
@@ -32,5 +34,51 @@ $(document).ready(function (){
 
 	$memberTags.click(function(e) {
 		var elementName = e.target.id.substr(5);
+		if (memberSet !== elementName) {
+
+			// Toggle button fill
+			$(e.target).toggleClass("primary");
+			$("#show-"+memberSet).toggleClass("primary");
+			$("#show-"+currentInstrument).toggleClass("primary");
+
+			// Toggle fadeout
+			var fadeOutButtons = "";
+			var fadeInButtons = "";
+			if (memberSet == "current") {
+				fadeOutButtons = "#show-piano, #show-strings, #show-winds, #show-triangle";
+			} else if (elementName == "current") {
+				fadeInButtons = "#show-piano, #show-strings, #show-winds, #show-triangle";
+			}
+
+			var fadeOutSelector = "";
+			var fadeInSelector = "";
+
+			if (elementName == "current") {
+				fadeInSelector = ".members > div";
+			} else if (elementName == "alumni") {
+				fadeInSelector = ".members > .alum";
+			}
+
+			if (memberSet == "current") {
+				fadeOutSelector = ".members > div";
+			} else if (memberSet == "alumni") {
+				fadeOutSelector = ".members > .alum";
+			}
+
+			if (fadeOutButtons != "") {
+				fadeOutSelector = fadeOutButtons + "," + fadeOutSelector;
+			}
+			if (fadeInButtons != "") {
+				fadeInSelector = fadeInButtons + "," + fadeInSelector;
+			}
+
+			$.when($(fadeOutSelector).fadeTo("fast", 0)).then(function() {
+				  $(fadeOutSelector).css("display", "none");
+				  $(fadeInSelector).fadeTo('fast', 1);
+				  currentInstrument = "all";
+				  memberSet = elementName;
+				  $("#show-all").addClass("primary");
+				});
+			}
 	})
 })
